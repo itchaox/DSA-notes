ec9eb69bb3a99b092fd1329e2287af065767662a
wangchao <wangchao2423@gmail.com>
2023-11-15 16:35:24 +0800

    feat(Tree): 完善删除操作

--- 06_Tree/01_BSTree.ts
+++ 06_Tree/01_BSTree.ts
@@ -3,7 +3,7 @@
  * @Author     : wangchao
  * @Date       : 2023-11-07 11:15
  * @LastAuthor : wangchao
- * @LastTime   : 2023-11-13 13:17
+ * @LastTime   : 2023-11-15 13:39
  * @desc       : 二叉搜索树
  */
 
@@ -13,6 +13,8 @@ class TreeNode<T> {
   value: T;
   left: TreeNode<T> | null = null;
   right: TreeNode<T> | null = null;
+  parent: TreeNode<T> | null = null;
+  isRight: boolean = false;
 
   constructor(value: T) {
     this.value = value;
@@ -21,6 +23,38 @@ class TreeNode<T> {
 
 class BSTree<T> {
   root: TreeNode<T> | null = null;
+
+  /**
+   * @desc  : 根据数据查询节点
+   * @param  {T} value：数据
+   * @return {any} 查询到的节点
+   */
+  private searchNode(value: T): TreeNode<T> | null {
+    let current = this.root;
+    let parent: TreeNode<T> | null = null;
+
+    while (current) {
+      if (current.value === value) return current;
+
+      // 保存父节点
+      parent = current;
+
+      // 比较后修改节点
+      if (current.value < value) {
+        current = current.right;
+      } else {
+        current = current.left;
+      }
+
+      // 更新当前节点的父节点
+      if (current) {
+        current.parent = parent;
+        // 子节点比父节点大，则是右节点
+        current.isRight = current.value > current.parent.value;
+      }
+    }
+    return null;
+  }
 
   // 打印树结构
   print() {
@@ -157,17 +191,86 @@ class BSTree<T> {
    * @return {any} 是否查到
    */
   search(value: T): boolean {
-    let current = this.root;
-    while (current) {
-      if (current.value === value) return true;
+    const current = this.searchNode(value);
+    return !!current;
+  }
 
-      if (current.value < value) {
-        current = current.right;
-      } else {
-        current = current.left;
+  /**
+   * @desc  : 获取后继节点
+   * @param  {TreeNode} delNode：删除节点
+   * @return {any} 后继节点
+   */
+  private getSuccessor(delNode: TreeNode<T>): TreeNode<T> {
+    // 后继节点一定在右子树
+    let current = delNode.right;
+    let successor: TreeNode<T> | null = null;
+    while (current) {
+      successor = current;
+      current = current.left;
+      if (current) {
+        current.parent = successor;
       }
     }
-    return false;
+
+    // 获取后继节点
+    if (successor !== delNode.right) {
+      // 后继节点有右节点时
+      successor!.parent!.left = successor?.right ?? null;
+
+      successor!.right = delNode.right;
+    }
+
+    // 必须做的操作，把删除节点的左节点，给到后继节点
+    successor!.left = delNode!.left;
+
+    return successor!;
+  }
+
+  /**
+   * @desc  : 删除节点
+   * @param  {T} value：节点数据
+   * @return {any}
+   */
+  remove(value: T): boolean {
+    const current = this.searchNode(value);
+    // 查找不到，则直接退出
+    if (!current) return false;
+
+    // 获取三个信息：当前节点、当前节点的父节点、当前节点是父节点的左节点还是右节点
+
+    let replaceNode: TreeNode<T> | null = null;
+
+    // FIXME 1. 删除叶子节点
+    if (current.right === null && current.left === null) {
+      replaceNode = null;
+    }
+
+    // FIXME 2. 只有一个节点，且是左节点
+    else if (current.right === null) {
+      replaceNode = current.left;
+    }
+
+    // FIXME 3. 只有一个节点，且是右节点
+    else if (current.left === null) {
+      replaceNode = current.right;
+    }
+
+    // FIXME 4. 有两个节点
+    else {
+      const successor = this.getSuccessor(current);
+      replaceNode = successor;
+    }
+
+    // 当前节点是根节点
+    if (current === this.root) {
+      this.root = replaceNode;
+    } else if (current.isRight) {
+      current.parent!.right = replaceNode;
+    } else {
+      current.parent!.left = replaceNode;
+    }
+
+    return true;
   }
 }
 
@@ -192,21 +295,12 @@ bsTree.insert(6);
 console.log("\n树结构打印如下：");
 bsTree.print();
 
-console.log("\n前序遍历结果如下：");
-bsTree.preOrderTraverse();
-
-console.log("\n中序遍历结果如下：");
-bsTree.inOrderTraverse();
-
-console.log("\n后序遍历结果如下：");
-bsTree.postOrderTraverse();
-
-console.log("\n层序遍历结果如下：");
-bsTree.levelOrderTraverse();
-
-console.log("树的最大值：", bsTree.maxValue);
-console.log("树的最小值：", bsTree.minValue);
-
 console.log(bsTree.search(10));
 console.log(bsTree.search(101));
 console.log(bsTree.search(25));
+
+bsTree.remove(11);
+bsTree.remove(12);
+bsTree.remove(5);
+
+bsTree.print();
