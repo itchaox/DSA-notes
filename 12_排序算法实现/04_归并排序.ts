0ca95e83bccae71a8cde9d2a46f1587b895df583
itchaox <1462831573@qq.com>
2024-05-05 23:35:21 +0800

    feat: 完善归并排序

--- 12_排序算法实现/04_归并排序.ts
+++ 12_排序算法实现/04_归并排序.ts
@@ -3,8 +3,8 @@
  * @Author     : itchaox
  * @Date       : 2024-04-05 16:27
  * @LastAuthor : itchaox
- * @LastTime   : 2024-04-05 16:51
- * @desc       :
+ * @LastTime   : 2024-05-05 23:34
+ * @desc       : 归并排序
  */
 
 import { testSort } from './00_utils';
@@ -15,17 +15,21 @@ function mergeSort(arr: number[]): number[] {
 
   // 1. 分解
   // 数组长度为 1，则退出递归
-  if (arr.length <= 1) return arr;
+  if (n <= 1) return arr;
 
   // 获取数组中间索引
-  const minIndex = Math.floor(n / 2);
+  const mid = Math.floor(n / 2);
 
   // 左数组
-  const leftArr = arr.slice(0, minIndex);
+  const leftArr = arr.slice(0, mid);
 
   // 右数组
-  const rightArr = arr.slice(minIndex);
+  const rightArr = arr.slice(mid);
 
+  // 当递归结束的时候，数组长度为 1，此时拿到这个数据然后再执行后续的合并操作
+  // 在这里会从数组为1的时候一直向上递归，返回排序好的数组，然后直到还有2组数组，把这2组数组再合并，即为最终结果
+
+  // 递归的操作需要注意，每个上面的操作都需要等下层的操作结束之后拿到结果，再执行后续的操作
   const newLeftArr = mergeSort(leftArr);
   const newRightArr = mergeSort(rightArr);
 
@@ -37,6 +41,7 @@ function mergeSort(arr: number[]): number[] {
   let j = 0;
 
   // 当一个数组遍历完，则退出循环
+  // 必须一个数组中有元素菜继续遍历
   while (i < newLeftArr.length && j < newRightArr.length) {
     // 将较小值的数组放入新数组，并移动对应指针
     if (newLeftArr[i] <= newRightArr[j]) {
@@ -51,11 +56,20 @@ function mergeSort(arr: number[]): number[] {
   // 当一个数组循环完之后，直接把另一个数组剩余的元素添加至新数组即可
   if (i < newLeftArr.length) {
     newArr.push(...newLeftArr.slice(i));
-  } else {
+  }
+
+  if (j < newRightArr.length) {
     newArr.push(...newRightArr.slice(j));
   }
 
   return newArr;
 }
 
-testSort(mergeSort);
+// 算法测试
+let arr = [10, 90, 20, 100, 50];
+console.log('排序前：', arr); // [ 10, 90, 20, 100, 50 ]
+
+let newArr = mergeSort(arr);
+console.log('排序后：', newArr); // [ 10, 20, 50, 90, 100 ]
+
+// testSort(mergeSort);
