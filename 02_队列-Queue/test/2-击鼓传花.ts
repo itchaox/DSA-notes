05c98759995334ef7920aeb1d72a527e396df2ae
Wang Chao <wangchao2423@gmail.com>
2024-09-07 19:10:50 +0800

    1

--- 02_队列-Queue/test/2-击鼓传花.ts
+++ 02_队列-Queue/test/2-击鼓传花.ts
@@ -3,7 +3,7 @@
  * @Author     : Wang Chao
  * @Date       : 2024-09-03 14:39
  * @LastAuthor : Wang Chao
- * @LastTime   : 2024-09-07 18:39
+ * @LastTime   : 2024-09-07 19:08
  * @desc       :
  */
 
@@ -12,15 +12,42 @@
 // FIXME 题目描述
 
 /**
- * 输入为 ['a', 'b', 'c', 'd', 'e'] 3
- * 遇到 3 则删除该元素，返回最后剩下的数据
+ *  输入: data = ['a', 'b', 'c', 'd', 'e'], num = 3
+ *  输出: 'd'
+ *
+ *  输入: data = ['1', '2', '3', '4'], num = 2
+ *  输出: '1'
+ * 从 1 开始数遇到 3 则删除该元素，返回最后剩下的数据
  */
 
 // FIXME [] 返回 ''
 
 // FIXME 实现
-function hotPhtato() {}
+function hotPhtato(arr: string[], num: number): string {
+  const n = arr.length;
+
+  if (n === 0) return '';
+
+  let queue: string[] = [];
+
+  for (const v of arr) {
+    queue.push(v);
+  }
+
+  while (queue.length > 1) {
+    for (let i = 1; i < num; i++) {
+      const v = queue.shift()!;
+      queue.push(v);
+    }
+
+    queue.shift();
+  }
+
+  return queue.shift()!;
+}
 
 // FIXME 验证
+console.log(hotPhtato(['a', 'b', 'c', 'd', 'e'], 3));
+console.log(hotPhtato(['1', '2', '3', '4'], 2));
 
 // FIXME 时间和空间复杂度分析
